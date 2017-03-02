857de577443191a40904a95a0ec92325a372ed4c
Kristin Caron <krissycaron@gmail.com>
2017-03-01 19:24:37 -0600

    added html branch

---
+++ cards.js
@@ -0,0 +1,30 @@
+
+//methods  innerHTML 
+
+
+// Potentially another way of doing it with another method read Duckett DOM Chapter.
+// To help find maybe easier or more effective ways of handling the issue. 
+
+
+//////////////////// JavaScript ////////////////
+
+
+/// Variables /// 
+
+var userInputFromDOM = 
+var outputCardToDOM = 
+
+
+
+
+//function to get the user input from user input string ....
+
+
+//functiion to return the information to the DOM in the form of a card. 
+
+
+
+
+
+
+
